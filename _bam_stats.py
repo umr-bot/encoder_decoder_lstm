af3ab7ac2bdfc57174667d7a1fd83fbf95ef2fca
Umr Barends <ubarends@gmail.com>
2022-10-09 21:31:24 +0200

    Made a data handling class. Busy mixing error sentences into non-error containg ones.

---
+++ _bam_stats.py
@@ -0,0 +1,108 @@
+# coding: utf-8
+from sort_error_types import DataHandler, distribute, split
+from collections import defaultdict
+from tqdm import tqdm
+
+data_handler = DataHandler()
+sorted_single_errs, err_counts = data_handler.get_err_counts_dicts()
+fold_lens = sum(range(10)) + sum(range(11,19)) +21+23+26+29, sum([31,32,33,36,44,48,60]), sum([73,86,92]), 503
+num_folds = 4
+fold1 = [*range(1,5)]#+[*range(11,19)]#+[21,23,26,29]
+fold1_dev=[5]
+fold1_test=[6]
+fold2 = [*range(7,11)]
+fold2_dev=[11]
+fold2_test=[12,13]
+fold3 = [21,23,26,29,31,32,33,36]
+fold3_dev=[44]
+fold3_test=[48,60,73]
+fold4 = [*range(14,15)]+[503]
+fold4_dev=[15,86]
+fold4_test=[16,17,18,92]
+
+fold_lst = [fold1,fold2,fold3,fold4]
+fold_devs = [fold1_dev,fold2_dev,fold3_dev,fold4_dev]
+fold_tests = [fold1_test,fold2_test,fold3_test,fold4_test]
+folds = defaultdict(list)
+for i in range(num_folds):
+    folds["fold"+str(i+1)].append(fold_lst[i])
+    folds["fold"+str(i+1)+"dev"].append(fold_devs[i])
+    folds["fold"+str(i+1)+"test"].append(fold_tests[i])
+
+err_folds=defaultdict(list)
+#err_folds=[[] for dummy in range(len(folds))]
+for single_err in sorted_single_errs:
+    err_tok = single_err[0][2][1]
+    err_count = err_counts[err_tok]
+    for folds_key in folds.keys():
+        if err_count in folds[folds_key][0]:
+            err_folds[folds_key].append(single_err)
+            #err_folds[fold_cnt].append(single_err)
+
+print("number of sentences with unequal sequence lengths vs total fold size per fold:")
+for fold_key in folds.keys():
+    err_fold = err_folds[fold_key]
+    y = [x for x in err_fold if len(x[0][0])!=len(x[0][1])]
+    print(f"{fold_key}: {len(y)}, {len(err_fold)}")
+
+# Sanity check to see that there is no overlapping of types between folds
+# Note however that lists in x below are token lists and not type lists
+x = [[] for dummy in range(num_folds)]
+for i in range(num_folds):
+    x[i] = [err_fold[0][2][1] for err_fold in err_folds["fold"+str(i+1)]]
+def rotate(l, n):
+    return l[n:] + l[:n]
+def flatten(l):
+    return [item for sublist in l for item in sublist]
+# All these statements should print True if all folds disjoint
+if set(tuple(flatten(x[1:4]))).isdisjoint(x[0]): print("fold1 disjoint of all other folds")
+if set(tuple(flatten(rotate(x,1)[1:4]))).isdisjoint(x[1]): print("fold2 disjoint of all other folds")
+
+if set(tuple(flatten(rotate(x,2)[1:4]))).isdisjoint(x[2]): print("fold3 disjoint of all other folds")
+
+if set(tuple(flatten(rotate(x,3)[1:4]))).isdisjoint(x[3]): print("fold4 disjoint of all other folds")
+
+def write_folds(err_folds):
+    for fold_key in err_folds.keys():
+        i = str(int(fold_key[4]))
+        fn = "bam_folds/"+"fold"+str(i)+"/"+fold_key
+        with open(fn,"w") as f:
+            trains = [x[0][0] for x in err_folds[fold_key]]
+            for train in trains: f.write(train+"\n")
+        norm_fn = "bam_folds/"+"norm_fold"+str(i)+"/"+fold_key
+        with open(norm_fn,"w") as f:
+            norm_trains = [x[0][1] for x in err_folds[fold_key]]
+            for norm_train in norm_trains: f.write(norm_train+"\n")
+
+def join_data(err_folds,without_err,norm_without_err):
+    err_folds, counts = zip(*err_folds)
+    errors,norm_errors,error_tups_reformed = zip(*err_folds)
+    assert(len(without_err)==len(norm_without_err))
+
+    sorted_list = []
+    if len(without_err) > len(err_folds): 
+        sorted_list = list(distribute(source_one=without_err,source_two=errors))
+        norm_sorted_list = list(distribute(source_one=norm_without_err,source_two=norm_errors))
+        print("Less sentences with errors than those without.")
+    elif len(without_err) == len(err_folds):
+        # Add empty sentences to source_one arguments for distribute function to
+        # work, source_one > source_two, hence we add empty sentence
+        sorted_list = distribute(source_one=without_err.append(''),source_two=errors)
+        norm_sorted_list = distribute(source_one=norm_without_err.append(''),source_two=norm_errors)
+        print("Sentences with errors the same amount as those without.")
+    else: # reverse order of arguments passed to distribute function
+          # as len(source_one) > len(source_two) for function to work
+        sorted_list = distribute(source_one=errors,source_two=without_err)
+        norm_sorted_list = distribute(source_one=norm_errors,source_two=norm_without_err)
+        print("More sentences with errors than those without.")
+
+    return sorted_list, norm_sorted_list
+
+#without_err_folds = split(without_err,k_folds=4)
+#norm_without_err_folds = split(norm_without_err,k_folds=4)
+
+#sorted_folds,norm_sorted_folds=[],[]
+#for     
+#    sorted_list,norm_sorted_list = join_data(err_folds["fold1"], without_err_folds[0], norm_without_err_folds[0])
+
+
--- sort_error_types.py
+++ sort_error_types.py
@@ -1,7 +1,23 @@
 # coding: utf-8
 from collections import Counter
-from eng_data.kfolds.balance_errors_in_dataset import Balancer
+from artificial_corrupted_eng_data.kfolds.balance_errors_in_dataset import Balancer
 import itertools
+import string
+
+def split(raw_data, k_folds=5):
+    data=[]
+    for text in raw_data:
+        new_text = ''.join(c for c in text if c not in string.punctuation)
+        data.append(new_text)
+    fold_len = int(len(data)/k_folds)
+    fold, folds = [],[]
+    for i in range(len(data)):
+        fold.append(data[i])
+        if (i+1) % fold_len == 0:
+            folds.append(fold)
+            fold = []
+    return folds
+
 def distribute(source_one, source_two, order=[]):
     step = (len(source_one) - 2)//(len(source_two) - 1)
     splice = source_one[1:-1] # grab all items except first one in source_one
@@ -11,14 +27,15 @@ def distribute(source_one, source_two, order=[]):
     # iters = reorder(list(zip(*iters)))
     # flatten iters appended by source_one[0]
     compressed = itertools.chain(source_one[0:1], zip(*iters))
-    unzipped = zip(compressed, source_two) 
+    unzipped = list(zip(compressed, source_two))
     # eg. continued: if source_two = [0,1,2,3] then 
     # unzipped = [((source_one,0),(a,b),(c,d),(e,f)), (0,1,2,3)]
     flattened = list(flatten(unzipped)) # flatten unzipped into a 1xn dim list
     # then flattened= [(source_one[0],1), ((a,b),1), ((c,d),2), ((e,f),3)]
     # Finally, append source_one[-1] to flattened, another way is to return
     # list(itertools.chain(flattened, source_one[-1:]))
-    return flattened.append(source_one[-1]) 
+    flattened.append(source_one[-1]) 
+    return flattened
 
 def flatten(lst):
     return sum(([x] if not isinstance(x, tuple) else flatten(x) for x in lst), [])
@@ -29,92 +46,69 @@ def reorder(lst,order):
     tmp.sort(key=lambda x: int(x[1]),reverse=True) # sort lst according 'order'
     return list(zip(*tmp))[0] # return only reordered lst list
 
-bal = Balancer(original_fn="eng_data/kfolds/balanced_err_eng_50000",normed_fn="eng_data/kfolds/balanced_eng_50000")
-with_err,norm_with_err,without_err,norm_without_err,err_tups = bal.check_err()
+class DataHandler:
 
-err = list(zip(with_err,norm_with_err))
-#####################################################################
-# Split sentences with multiple errors into single error sentences
-single_errs=[]
-j=0
-for i in range(len(err)):
-    sent, norm_sent = err[i][0], err[i][1]
-    toks, ntoks = sent.split(), norm_sent.split()
-    for tok_cnt, ntok_cnt in zip(range(len(toks)), range(len(ntoks))):
-        tok, ntok = toks[tok_cnt], ntoks[ntok_cnt]
-        if tok != ntok: # for every error token in sentence
-            tmp = ntoks.copy()
-            tmp[ntok_cnt] = toks[tok_cnt] # insert error into normed sentence
-            tmp = " ".join([x for x in tmp])
-            ntoks_sent = " ".join([x for x in ntoks])
-            single_errs.append((tmp, ntoks_sent,(tok,ntok)))
-            j+=1
-            #print(f"tok_cnt:{tok_cnt}, tok:{tok}, ntok:{ntok}, j:{j}")
-    #print(f"\nlen of toks:{len(toks)}")
-#####################################################################
-# Get counts of word frequencies
-toks, ntoks = [],[]
-for i in range(len(single_errs)):
-    for tok, ntok in zip(single_errs[i][0].split(), single_errs[i][1].split()):
-        if tok != ntok: 
-            toks.append(tok)
-            ntoks.append(ntok)
+    def __init__(self,original_fn="data/balanced_all",normed_fn="data/balanced_norm_all"):
+        #bal = Balancer(original_fn="eng_data/kfolds/balanced_err_eng_50000",normed_fn="eng_data/kfolds/balanced_eng_50000")
+        bal = Balancer(original_fn=original_fn,normed_fn=normed_fn)
+        with_err,norm_with_err,without_err,norm_without_err,err_tups = bal.check_err()
+        self.without_err,self.norm_without_err=without_err,norm_without_err
+        self.sorted_single_errs,self.err_types,self.err_counts = self.create_counts_dicts(with_err, norm_with_err)
+        #self.sorted_single_,self.types,self.counts = self.create_counts_dicts(without_err, norm_without_err)
+    
+    def get_err_counts_dicts(self):
+        return self.sorted_single_errs,self.err_counts
 
-type_count = Counter(tok)
-ntype_count = Counter(ntoks)
-#####################################################################
-# Assign the counts of the word errorr to the respective sentences
-sorted_single_errs=[]
-for single_err in single_errs:
-    single_err_tup = single_err #tuple of sentence and its normed form
-    single_err_cnt = ntype_count[single_err[2][1]]
-    sorted_single_errs.append((single_err_tup, single_err_cnt))
-sorted_single_errs.sort(key=lambda x: x[1], reverse=True)
+    def create_counts_dicts(self,data,norm_data):
+        err = list(zip(data,norm_data))
+        #####################################################################
+        # Split sentences with multiple errors into single error sentences
+        single_errs=[]
+        j=0
+        for i in range(len(err)):
+            sent, norm_sent = err[i][0], err[i][1]
+            toks, ntoks = sent.split(), norm_sent.split()
+            for tok_cnt, ntok_cnt in zip(range(len(toks)), range(len(ntoks))):
+                tok, ntok = toks[tok_cnt], ntoks[ntok_cnt]
+                if tok != ntok: # for every error token in sentence
+                    tmp = ntoks.copy()
+                    tmp[ntok_cnt] = toks[tok_cnt] # insert error into normed sentence
+                    tmp = " ".join([x for x in tmp])
+                    ntoks_sent = " ".join([x for x in ntoks])
+                    single_errs.append((tmp, ntoks_sent,(tok,ntok)))
+                    j+=1
+                    #print(f"tok_cnt:{tok_cnt}, tok:{tok}, ntok:{ntok}, j:{j}")
+            #print(f"\nlen of toks:{len(toks)}")
+        #####################################################################
+        # Get counts of word frequencies
+        toks, ntoks = [],[]
+        for i in range(len(single_errs)):
+            for tok, ntok in zip(single_errs[i][0].split(), single_errs[i][1].split()):
+                if tok != ntok: 
+                    toks.append(tok)
+                    ntoks.append(ntok)
 
-cur_word=sorted_single_errs[0][0][2][1]
-norm_cnt = 0
-err_dict = dict()
-err_counts = Counter()
-for s in sorted_single_errs:
-    norm_word = s[0][2][1]
-#    if cur_word != norm_word:
-#        err_dict[cur_word] = norm_cnt
-#        cur_word = norm_word
-#        norm_cnt += 0
-#    else: 
-#        err_counts[cur_word].update()
-#        norm_cnt+=1
-    err_counts.update([norm_word])
+        type_count = Counter(tok)
+        ntype_count = Counter(ntoks)
+        #####################################################################
+        # Assign the counts of the word error to the respective sentences
+        sorted_single_errs=[]
+        for single_err in single_errs:
+            single_err_tup = single_err #tuple of sentence and its normed form
+            single_err_cnt = ntype_count[single_err[2][1]]
+            sorted_single_errs.append((single_err_tup, single_err_cnt))
+        sorted_single_errs.sort(key=lambda x: x[1], reverse=True)
 
-#err_sorted=[]
-#for i in range(len(err)):
-#    # iterate over sentences in err list
-#    tok_count = 0
-#    for tok, ntok in zip(err[i][0].split(), err[i][1].split()):
-#        # iterate over tokens in sentence
-#        if tok != ntok:
-#            # ADD the count of a misspelled token to tok_count
-#            tok_count += ntoks_count[ntok]
-#
-#    err_sorted.append((err[i], tok_count))
-#    
-#err_sorted.sort(key=lambda x: x[1], reverse=True)
+        #####################################################################
+        # Get number of word tokens associated with types in ntype_count
+        err_counts = Counter()
+        for s in sorted_single_errs:
+            norm_word = s[0][2][1] # normed word token associated with word type
+            err_counts.update([norm_word])
+        err_types, err_type_count = zip(*err_counts.items())
 
-#err_sorted_, counts = zip(*err_sorted) # TODO: include counts in sorting below
-#errors,norm_errors = zip(*err_sorted_)
-#sorted_list = []
-#if len(without_err) > len(err_sorted): 
-#    sorted_list = list(distribute(source_one=without_err,source_two=errors))
-#    norm_sorted_list = list(distribute(source_one=norm_without_err,source_two=norm_errors))
-#
-#elif len(without_err) == len(err_sorted):
-#    # Add empty sentences to source_one arguments for distribute function to
-#    # work, source_one > source_two, hence we add empty sentence
-#    sorted_list = distribute(source_one=without_err.append(''),source_two=errors)
-#    norm_sorted_list = distribute(source_one=norm_without_err.append(''),source_two=norm_errors)
-#
-#else: # reverse order of arguments passed to distribute function
-#      # as len(source_one) > len(source_two) for function to work
-#    sorted_list = distribute(source_one=errors,source_two=without_err)
-#    norm_sorted_list = distribute(source_one=norm_errors,source_two=norm_without_err)
-#
+        return sorted_single_errs,err_types,err_counts
+
+    def filter_word_types(self):
+        # Filter without err according to err types
+        pass
