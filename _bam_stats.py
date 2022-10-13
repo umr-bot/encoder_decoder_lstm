68173ea10d66fa88a636da7c06fbe3b6bf3abb37
Umr Barends <ubarends@gmail.com>
2022-10-14 00:30:59 +0200

    Intermediate update with function make_ana_folds.

--- _bam_stats.py
+++ _bam_stats.py
@@ -3,84 +3,144 @@ from sort_error_types import DataHandler, distribute, split
 from collections import defaultdict
 from tqdm import tqdm
 
-data_handler = DataHandler()
-sorted_single_errs, err_counts = data_handler.get_err_counts_dicts()
-fold_lens = sum(range(10)) + sum(range(11,19)) +21+23+26+29, sum([31,32,33,36,44,48,60]), sum([73,86,92]), 503
-num_folds = 4
-fold1 = [*range(1,5)]#+[*range(11,19)]#+[21,23,26,29]
-fold1_dev=[5]
-fold1_test=[6]
-fold2 = [*range(7,11)]
-fold2_dev=[11]
-fold2_test=[12,13]
-fold3 = [21,23,26,29,31,32,33,36]
-fold3_dev=[44]
-fold3_test=[48,60,73]
-fold4 = [*range(14,15)]+[503]
-fold4_dev=[15,86]
-fold4_test=[16,17,18,92]
-
-fold_lst = [fold1,fold2,fold3,fold4]
-fold_devs = [fold1_dev,fold2_dev,fold3_dev,fold4_dev]
-fold_tests = [fold1_test,fold2_test,fold3_test,fold4_test]
-folds = defaultdict(list)
-for i in range(num_folds):
-    folds["fold"+str(i+1)].append(fold_lst[i])
-    folds["fold"+str(i+1)+"dev"].append(fold_devs[i])
-    folds["fold"+str(i+1)+"test"].append(fold_tests[i])
-
-err_folds=defaultdict(list)
-#err_folds=[[] for dummy in range(len(folds))]
-for single_err in sorted_single_errs:
-    err_tok = single_err[0][2][1]
-    err_count = err_counts[err_tok]
-    for folds_key in folds.keys():
-        if err_count in folds[folds_key][0]:
-            err_folds[folds_key].append(single_err)
-            #err_folds[fold_cnt].append(single_err)
-
-print("number of sentences with unequal sequence lengths vs total fold size per fold:")
-for fold_key in folds.keys():
-    err_fold = err_folds[fold_key]
-    y = [x for x in err_fold if len(x[0][0])!=len(x[0][1])]
-    print(f"{fold_key}: {len(y)}, {len(err_fold)}")
-
-# Sanity check to see that there is no overlapping of types between folds
-# Note however that lists in x below are token lists and not type lists
-x = [[] for dummy in range(num_folds)]
-for i in range(num_folds):
-    x[i] = [err_fold[0][2][1] for err_fold in err_folds["fold"+str(i+1)]]
 def rotate(l, n):
     return l[n:] + l[:n]
 def flatten(l):
     return [item for sublist in l for item in sublist]
-# All these statements should print True if all folds disjoint
-if set(tuple(flatten(x[1:4]))).isdisjoint(x[0]): print("fold1 disjoint of all other folds")
-if set(tuple(flatten(rotate(x,1)[1:4]))).isdisjoint(x[1]): print("fold2 disjoint of all other folds")
+class BamStats:
 
-if set(tuple(flatten(rotate(x,2)[1:4]))).isdisjoint(x[2]): print("fold3 disjoint of all other folds")
+    def __init__(self):
+        self.data_handler = DataHandler()
+        self.sorted_single_errs, self.err_counts = self.data_handler.get_err_counts_dicts()
+        self.make_folds()
+        self.make_err_folds()
+        self.make_ana_folds()
 
-if set(tuple(flatten(rotate(x,3)[1:4]))).isdisjoint(x[3]): print("fold4 disjoint of all other folds")
+    def make_folds(self):
+        """Make folds dict containing counts of error types in corpus"""
+        fold_lens = sum(range(10)) + sum(range(11,19)) +21+23+26+29, sum([31,32,33,36,44,48,60]), sum([73,86,92]), 503
+        self.num_folds = 4
+        fold1 = [*range(1,5)]#+[*range(11,19)]#+[21,23,26,29]
+        fold1_dev=[5]
+        fold1_test=[6]
+        fold2 = [*range(7,11)]
+        fold2_dev=[11]
+        fold2_test=[12,13]
+        fold3 = [21,23,26,29,31,32,33,36]
+        fold3_dev=[44]
+        fold3_test=[48,60,73]
+        fold4 = [*range(14,15)]+[503]
+        fold4_dev=[15,86]
+        fold4_test=[16,17,18,92]
 
-ll = list(set([tok for toks in data_handler.without_err for tok in toks.split()]))
-interval = int(len(ll)/num_folds)
-ll_spliced=[]
-tups = defaultdict(set)
-for i in range(num_folds):
-    if i == num_folds-1: ll_spliced = ll[i*interval:]
-    else: ll_spliced = ll[i*interval:(i+1)*interval]
-    # Add 250 tuples from correct tokens to dev and test
-    dev = list(tok[0][2][1] for tok in err_folds["fold"+str(i+1)+"dev"])
-    test = list(tok[0][2][1] for tok in err_folds["fold"+str(i+1)+"test"])
-    dev_test = dev + test
-    tups["fold"+str(i+1)+"dev"] = set([tup[0][2] for tup in err_folds["fold"+str(i+1)+"dev"]] + [(x,x) for x in ll_spliced[-1000:-500]])
-    tups["fold"+str(i+1)+"test"] = set([tup[0][2] for tup in err_folds["fold"+str(i+1)+"test"]] + [(x,x) for x in ll_spliced[-500:]])
-    tups["fold"+str(i+1)] = set([tup[0][2] for tup in err_folds["fold"+str(i+1)]])
-    # note union does not do in place updates, hence have to assign result
-    tups["fold"+str(i+1)]=tups["fold"+str(i+1)].union([(x,x) for x in ll_spliced[0:-1000] if x not in dev_test])
+        fold_lst = [fold1,fold2,fold3,fold4]
+        fold_devs = [fold1_dev,fold2_dev,fold3_dev,fold4_dev]
+        fold_tests = [fold1_test,fold2_test,fold3_test,fold4_test]
+        folds = defaultdict(list)
+        for i in range(self.num_folds):
+            folds["fold"+str(i+1)].append(fold_lst[i])
+            folds["fold"+str(i+1)+"dev"].append(fold_devs[i])
+            folds["fold"+str(i+1)+"test"].append(fold_tests[i])
+        self.folds=folds
 
-root_dir = "bam_folds/folds/"
-for key_name in tups.keys():
-    with open(root_dir+key_name, "w") as f:
-        for tup in tups[key_name]: f.write(tup[0]+','+tup[1]+"\n")
+    def make_err_folds(self, fold_check=False):
+        self.err_folds=defaultdict(list)
+        #err_folds=[[] for dummy in range(len(self.folds))]
+        for single_err in self.sorted_single_errs:
+            err_tok = single_err[0][2][1]
+            err_count = self.err_counts[err_tok]
+            for folds_key in self.folds.keys():
+                if err_count in self.folds[folds_key][0]:
+                    self.err_folds[folds_key].append(single_err)
+                    #err_folds[fold_cnt].append(single_err)
+        if fold_check:
+            print("number of sentences with unequal sequence lengths vs total fold size per fold:")
+            for fold_key in self.folds.keys():
+                err_fold = self.err_folds[fold_key]
+                y = [x for x in err_fold if len(x[0][0])!=len(x[0][1])]
+                print(f"{fold_key}: {len(y)}, {len(err_fold)}")
 
+            # Sanity check to see that there is no overlapping of types between folds
+            # Note however that lists in x below are token lists and not type lists
+            x = [[] for dummy in range(self.num_folds)]
+            for i in range(self.num_folds):
+                x[i] = [err_fold[0][2][1] for err_fold in self.err_folds["fold"+str(i+1)]]
+
+            # All these statements should print True if all folds disjoint
+            if set(tuple(flatten(x[1:4]))).isdisjoint(x[0]): print("fold1 disjoint of all other folds")
+            if set(tuple(flatten(rotate(x,1)[1:4]))).isdisjoint(x[1]): print("fold2 disjoint of all other folds")
+
+            if set(tuple(flatten(rotate(x,2)[1:4]))).isdisjoint(x[2]): print("fold3 disjoint of all other folds")
+
+            if set(tuple(flatten(rotate(x,3)[1:4]))).isdisjoint(x[3]): print("fold4 disjoint of all other folds")
+
+    def make_folds_for_enc_dec_model(self):
+        """Input: DataHandler object data_handler,
+                  number of folds num_folds
+                  and defaultdict err_folds with key as fold name and values
+                  a 4-tuple in the form of:
+            ((errorful_sentece,normed_sentence,tuple_of_err_and_norm_word), frequency_count_of_norm_word)
+        """
+        ll = list(set([tok for toks in self.data_handler.without_err for tok in toks.split()]))
+        interval = int(len(ll)/self.num_folds)
+        ll_spliced=[]
+        tups = defaultdict(set)
+        for i in range(num_folds):
+            if i == num_folds-1: ll_spliced = ll[i*interval:]
+            else: ll_spliced = ll[i*interval:(i+1)*interval]
+            # Add 250 tuples from correct tokens to dev and test
+            dev = list(tok[0][2][1] for tok in self.err_folds["fold"+str(i+1)+"dev"])
+            test = list(tok[0][2][1] for tok in self.err_folds["fold"+str(i+1)+"test"])
+            dev_test = dev + test
+            tups["fold"+str(i+1)+"dev"] = set([tup[0][2] for tup in self.err_folds["fold"+str(i+1)+"dev"]] + [(x,x) for x in ll_spliced[-1000:-500]])
+            tups["fold"+str(i+1)+"test"] = set([tup[0][2] for tup in self.err_folds["fold"+str(i+1)+"test"]] + [(x,x) for x in ll_spliced[-500:]])
+            tups["fold"+str(i+1)] = set([tup[0][2] for tup in self.err_folds["fold"+str(i+1)]])
+            # note union does not do in place updates, hence have to assign result
+            tups["fold"+str(i+1)]=tups["fold"+str(i+1)].union([(x,x) for x in ll_spliced[0:-1000] if x not in dev_test])
+        return tups
+    
+    def write_tups(self,tups,out_fn="bam_folds/folds/"):
+        for key_name in tups.keys():
+            with open(out_fn+key_name, "w") as f:
+                for tup in tups[key_name]: f.write(tup[0]+','+tup[1]+"\n")
+    
+    def make_ana_folds(self):
+        """Make data anagram hashing compatible"""
+        self.ana_folds = []
+        self.norm_ana_folds = []
+        for i in range(1,self.num_folds+1):
+            train=[line[0][0] for line in self.err_folds["fold"+str(i)]]
+            dev=[line[0][0] for line in self.err_folds["fold"+str(i)+"dev"]]
+            test=[line[0][0] for line in self.err_folds["fold"+str(i)+"test"]]
+            norm_train=[line[0][1] for line in self.err_folds["fold"+str(i)]]
+            norm_dev=[line[0][1] for line in self.err_folds["fold"+str(i)+"dev"]]
+            norm_test=[line[0][1] for line in self.err_folds["fold"+str(i)+"test"]]
+            
+            self.ana_folds.append(train+dev+test)
+            self.norm_ana_folds.append(norm_train+norm_dev+norm_test)
+
+#        bi_grams,norm_bi_grams=[],[]
+#        for line,norm_line in zip(bam_stats.data_handler.without_err,bam_stats.data_handler.norm_without_err):
+#            toks,norm_toks = line.split(), norm_line.split()
+#            for i in range(len(toks)-1):
+#                bi_grams.append((toks[i],toks[i+1]))
+#                norm_bi_grams.append((norm_toks[i],norm_toks[i+1]))
+
+if __name__ == "__main__":
+   bam_stats = BamStats() 
+#lines = [line for line in data_handler.without_err]
+#lines_set = [set(line.split()) for line in data_handler.without_err]
+#lines_tup = [(line,set(line.split())) for line in data_handler.without_err]
+#l_fold1 = []
+#tot_set = set()
+#for line, line_set in lines_tup:
+#    if line_set.isdisjoint(tot_set): l_fold1.append((line,line_set))
+#    tot_set = tot_set.union(line_set)
+#    
+#lines_tup2 = []
+#for line, line_set in lines_tup:
+#    for l_fold, l_fold_set in l_fold1:
+#        # use frozenset so that we can take set of the sets in lines_tup2
+#        if line_set.isdisjoint(l_fold_set): lines_tup2.append((line, frozenset(line_set)))
+#
+#len(set(lines_tup2))
