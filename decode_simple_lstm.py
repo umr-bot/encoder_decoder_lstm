894cf99c953d901d299e2b5a9b2bbe1468c536bf
Umr Barends <ubarends@gmail.com>
2023-03-07 15:50:42 +0200

    Updated bam data

--- decode_simple_lstm.py
+++ decode_simple_lstm.py
@@ -11,15 +11,7 @@ with open("eng_data_common_misspellings/folds/enc_dec_folds/enc_dec_fold1/train"
     train_tups = [line.strip('\n').split(',') for line in f]
 train_dec_tokens, train_tokens = zip(*train_tups)
 
-with open("eng_data_common_misspellings/folds/enc_dec_folds/enc_dec_fold1/train") as f:
-    train_tups = [line.strip('\n').split(',') for line in f]
-train_dec_tokens, train_tokens = zip(*train_tups)
-
-with open("eng_data_common_misspellings/folds/enc_dec_folds/enc_dec_fold1/train") as f:
-    test_tups = [line.strip('\n').split(',') for line in f]
-test_dec_tokens, test_tokens = zip(*test_tups)
-
-with open("eng_data_common_misspellings/folds/enc_dec_folds/enc_dec_fold1/train") as f:
+with open("eng_data_common_misspellings/folds/enc_dec_folds/enc_dec_fold1/test") as f:
     test_tups = [line.strip('\n').split(',') for line in f]
 test_dec_tokens, test_tokens = zip(*test_tups)
 
@@ -29,7 +21,7 @@ nb_input_chars = len(input_chars)
 
 maxlen = max([len(token) for token in test_tokens]) + 2
 
-model = load_model("simple_lstm_checkpoints/lstm_model_epoch_98.h5", custom_objects={'recall': recall, "f1_score": f1_score})
+model = load_model("simple_lstm_checkpoints/lstm_epoch_99.h5", custom_objects={'recall': recall, "f1_score": f1_score})
 start_ind,end_ind=20,30
 
 test_encoder, test_decoder, test_target = transform2( test_tokens[start_ind:end_ind], maxlen, shuffle=False, dec_tokens=test_dec_tokens[start_ind:end_ind])
@@ -49,9 +41,6 @@ for index in indices:
 input_sequences = batch(input_tokens, maxlen, ctable, nb_examples, reverse)
 input_sequences = next(input_sequences)
 
-# Encode the input as state vectors.    
-states_value = model.predict(input_sequences)
-
 # Create batch of empty target sequences of length 1 character.
 target_sequences = np.zeros((nb_examples, 1, ctable.size))
 # Populate the first element of target sequence
@@ -64,7 +53,7 @@ target_sequences[:, 0, ctable.char2index[SOS]] = 1.0
 decoded_tokens = [''] * nb_examples
 for _ in range(maxlen):
     # `char_probs` has shape: (nb_examples, 1, nb_target_chars)
-    char_probs = model.predict([target_sequences])
+    char_probs = model.predict(target_sequences)
 
     # Reset the target sequences.
     target_sequences = np.zeros((nb_examples, 1, ctable.size))
