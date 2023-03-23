51e49cdb38587d975cdb939a24882b1587aef392
Umr Barends <ubarends@gmail.com>
2023-03-23 12:07:27 +0200

    Updated the make_tris script to write out made fold splits..

--- eng_v2/make_tris.py
+++ eng_v2/make_tris.py
@@ -1,48 +1,49 @@
 # coding: utf-8
 from tqdm import tqdm
+from collections import  Counter
 
 # Read in text data into list of sentences
-with open("eng_za/trigram_tokens") as f:
-    tris=[]
+with open("eng_za/eng_za") as f:
+    text=[]
     for line in f:
-        tris.append([tok for tok in line.strip('\n').split(',') if set("[]-<>_").isdisjoint(set(tok))])
-tris = list(set(tuple(tri) for tri in tris)) # only keep types
+        text.append([tok for tok in line.strip('\n').split() if set("[]-<>_").isdisjoint(set(tok))])
 ####################################################
 # Create and fill trigram list of all trigrams in text
-#tris = []
-#for line in text:
-#    for i in range(len(line)-2):
-#        tris.append((line[i],line[i+1],line[i+2]))
+tris = []
+for line in text:
+    for i in range(len(line)-2):
+        tris.append((line[i],line[i+1],line[i+2]))
 ####################################################
-# Read in unigram fold data into parts variable
-parts=[[],[],[],[],[]]
-for i in range(5):
-    with open("folds/part"+str(i+1)) as f:
-        parts[i] = [tuple(line.strip('\n').split(','))[0] for line in f]
-        parts[i] = list(set(parts[i]))
-####################################################
-# Create folds out of trigrams list and unigram folds stored in parts variable
-folds=[[],[],[],[],[]]
-for i in range(5):
-    for tok in tqdm(parts[i],desc=f"Fold {i}"):
-        for tri in tris:
-            if tri[1] == tok: folds[i].append(tri)
+type_folds=[[] for i in range(5)]
+tris_set = list(set(tris))
+div = len(tris_set)//5
+for i in range(5): type_folds[i] = tris_set[i*div:(i+1)*div]
+print("Number of trigram types per fold:",str(div))
 
+cnts = Counter(tris)
+folds = [[] for i in range(5)]
+for i in range(5):
+    for tri in type_folds[i]:
+        for blank in range(cnts[tuple(tri)]):
+            folds[i].append(tri)
+print("Printing number of trigram tokens per fold")
+for i in range(5): print(f"Fold {i+1}: {len(folds[i])}") 
+
+# Read in unigram fold data into parts variable
+#parts=[[],[],[],[],[]]
+#for i in range(5):
+#    with open("folds/part"+str(i+1)) as f:
+#        parts[i] = [tuple(line.strip('\n').split(','))[1] for line in f]
+#        parts[i] = list(set(parts[i]))
+#####################################################
+## Create folds out of trigrams list and unigram folds stored in parts variable
+#folds=[[],[],[],[],[]]
+#for i in range(5):
+#    for tok in tqdm(parts[i],desc=f"Fold {i}"):
+#        for tri in tris:
+#            if tri[1] == tok: folds[i].append(tri)
+#
 for i in range(5):
     with open("trigrams/fold"+str(i+1),'w') as f:
-        for tri in folds[i]: f.write(tri[0]+','+tri[1]+','+tri[2]+'\n')
-# remove trigram  overlap in folds
-m = [[] for blank in range(5)]
-x = m.copy()
-for fold_cnt in range(5): m[fold_cnt] = set(tuple(tri) for tri in folds[fold_cnt])
-for i in range(5):
-    for j in range(5):
-        if m[i].isdisjoint(m[j]) == False and i!=j:
-            print("False",end=" ")
-            x[i] = m[i] - m[i].intersection(m[j])
-    print("------------")
-# sanity checkof fold independency
-for j in range(5):
-    for i in range(5): print(x[j].isdisjoint(x[i]),end=" ")
-    print("------------")    
+        for tri in folds[i]: f.write(tri[0]+' '+tri[1]+' '+tri[2]+'\n')
 
